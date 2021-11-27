46572beb57f2b79a3b3166ce1c200fc408eb2c1b
sarah hu <sarahhu@sarahdeMacBook-Air.local>
2021-11-26 21:11:46 -0800

    added noTeam output and line of code

--- data_analysis/nba_salary_analysis.py
+++ data_analysis/nba_salary_analysis.py
@@ -7,67 +7,84 @@ from pyspark.sql.functions import avg, sum, count, when
 from pyspark.sql.functions import avg, col, when
 from pyspark.sql.window import Window
 
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import StringIndexer, VectorAssembler, SQLTransformer
+from pyspark.ml.regression import GBTRegressor, RandomForestRegressor, DecisionTreeRegressor
+from pyspark.ml.evaluation import RegressionEvaluator
 
-def main(salary_path, salary_player_path, player_summary_path, player_info_path, injury_path, output):
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import pip
+pip.main(['install','seaborn'])
+import seaborn as sns
+
+
+salary_schema = types.StructType([
+    types.StructField('PLAYER_NAME', types.StringType()),
+    types.StructField('season', types.IntegerType()),
+    types.StructField('PLAYER_ID', types.StringType()),
+    types.StructField('TEAM_ID', types.StringType()),
+    types.StructField('avg(FGM)', types.FloatType()),
+    types.StructField('avg(FGA)', types.FloatType()),
+    types.StructField('avg(FG3M)', types.FloatType()),
+    types.StructField('avg(FG3A)', types.FloatType()),
+    types.StructField('avg(FTM)', types.FloatType()),
+    types.StructField('avg(FTA)', types.FloatType()),
+    types.StructField('avg(OREB)', types.FloatType()),
+    types.StructField('avg(DREB)', types.FloatType()),
+    types.StructField('avg(REB)', types.FloatType()),
+    types.StructField('avg(AST)', types.FloatType()),
+    types.StructField('avg(STL)', types.FloatType()),
+    types.StructField('avg(BLK)', types.FloatType()),
+    types.StructField('avg(TO)', types.FloatType()),
+    types.StructField('avg(PF)', types.FloatType()),
+    types.StructField('avg(PTS)', types.FloatType()),
+    types.StructField('avg(PLUS_MINUS)', types.FloatType()),
+    types.StructField('sum(FGM)', types.FloatType()),
+    types.StructField('sum(FGA)', types.FloatType()),
+    types.StructField('sum(FG3M)', types.FloatType()),
+    types.StructField('sum(FG3A)', types.FloatType()),
+    types.StructField('sum(FTM)', types.FloatType()),
+    types.StructField('sum(FTA)', types.FloatType()),
+    types.StructField('sum(OREB)', types.FloatType()),
+    types.StructField('sum(DREB)', types.FloatType()),
+    types.StructField('sum(REB)', types.FloatType()),
+    types.StructField('sum(AST)', types.FloatType()),
+    types.StructField('sum(STL)', types.FloatType()),
+    types.StructField('sum(BLK)', types.FloatType()),
+    types.StructField('sum(TO)', types.FloatType()),
+    types.StructField('sum(PF)', types.FloatType()),
+    types.StructField('sum(PTS)', types.FloatType()),
+    types.StructField('sum(PLUS_MINUS)', types.FloatType()),
+    types.StructField('sum(ifminute)', types.FloatType()),
+    types.StructField('avg(seconds)', types.FloatType()),
+    types.StructField('sum(seconds)', types.FloatType()),
+    types.StructField('age', types.FloatType()),
+    types.StructField('player_height', types.FloatType()),
+    types.StructField('player_weight', types.FloatType()),
+    types.StructField('injuries', types.FloatType()),
+    types.StructField('salary', types.FloatType())
+])
+
+def main(salary_path):
     # main logic starts here
-    salary = spark.read.option("delimiter", ",").option("header", "true").csv(salary_path)
-    salary_player = spark.read.option("delimiter", ",").option("header", "true").csv(salary_player_path)
-    player_summary = spark.read.option("delimiter", ",").option("header", "true").csv(player_summary_path)
-    player_info = spark.read.option("delimiter", ",").option("header", "true").csv(player_info_path)
-    injury = spark.read.option("delimiter", ",").option("header", "true").csv(injury_path)
+    #salary_path = "/Users/sarahhu/Desktop/SFUgrad/CMPT732/732Project/732-project/data/clean_data/salary_etl"
+    salary = spark.read.option("delimiter", ",").option("header", "true").csv(salary_path, schema=salary_schema)
 
-    salary = salary.select('player_id', 'salary', 'season_start')
-    salary_player = salary_player.select('_id', 'name')
-    salary_df = salary.join(salary_player, salary['player_id'] == salary_player['_id'], 'left')
-    salary_df = salary_df.select('name', 'season_start', 'salary')
-    player_info = player_info.withColumn("player_name", functions.translate(functions.col("player_name"), ".", ""))
-    player_info = player_info.drop('player_position', 'team_abbreviation', 'player_name', 'draft_year', 'draft_round',
-                                   'draft_number')
-    salary_df = salary_df.withColumn("name", functions.translate(functions.col("name"), ".", ""))
-    salary_df = salary_df.withColumnRenamed("name", "PLAYER_NAME").withColumnRenamed("season_start", "season")
-    # player_info.filter(col("player_name").contains(".")).show() #check no more dots in name
-    # salary_df.filter(col("name").contains(".")).show()
+    salary_df = salary.toPandas()
+    print(salary_df.corr())
 
-    player_summary = player_summary.withColumnRenamed("year", "season")
-    joinedDF = player_summary.join(player_info, ['PLAYER_ID', 'season'], 'left')
-
-    injury = injury.withColumn('Date', functions.substring('Date', 1, 4))
-    injury = injury.withColumnRenamed("Date", "season")
-    injury = injury.withColumn("Relinquished", functions.translate(functions.col("Relinquished"), ".", ""))
-    injury = injury.withColumnRenamed("Relinquished", "PLAYER_NAME")
-    injury = injury.withColumn("PLAYER_NAME", functions.translate(functions.col("PLAYER_NAME"), ".", ""))
-
-    injury = injury.withColumn("if_injury", when(injury["Notes"].isNull(), 0).otherwise(1))
-    injuryGP = injury.groupBy('season', 'PLAYER_NAME').agg(sum('if_injury').alias('injuries'))
-
-    joinedDF = joinedDF.join(injuryGP, ['PLAYER_NAME', 'season'], 'left')
-    joinedDF = joinedDF.join(salary_df, ['PLAYER_NAME', 'season'], 'left')
-    # joinedDF = joinedDF.drop("player_name")
-    joinedDF = joinedDF.orderBy('season', 'PLAYER_NAME')
-    joinedDF = joinedDF.filter(joinedDF['avg(FGM)'].isNotNull() & joinedDF['salary'].isNotNull())
-    # from pyspark.sql.types import DoubleType
-    # joinedDF = joinedDF.withColumn("age",col("age").cast(DoubleType()))
-    w = Window().partitionBy('PLAYER_NAME')
-    joinedDF2 = joinedDF.withColumn('age', when(col('age').isNull(), avg(col('age')).over(w)).otherwise(col('age')))
-    joinedDF2 = joinedDF2.withColumn('player_height',
-                                     when(col('player_height').isNull(), avg(col('player_height')).over(w)).otherwise(
-                                         col('player_height')))
-    joinedDF2 = joinedDF2.withColumn('player_weight',
-                                     when(col('player_weight').isNull(), avg(col('player_weight')).over(w)).otherwise(
-                                         col('player_weight')))
-    joinedDF2.coalesce(1).write.option("header", "true").csv(output, mode='overwrite')
-
+    sns.set_style("white")
+    sns.heatmap(salary_df.corr())
+    plt.show()
 
 if __name__ == '__main__':
     salary_path = sys.argv[1]
-    salary_player_path = sys.argv[2]
-    player_summary_path = sys.argv[3]
-    player_info_path = sys.argv[4]
-    injury_path = sys.argv[5]
-    output = sys.argv[6]
+    #output = sys.argv[2]
     spark = SparkSession.builder.appName('NBA ETL process').getOrCreate()
     assert spark.version >= '3.0'  # make sure we have Spark 3.0+
     spark.sparkContext.setLogLevel('WARN')
     sc = spark.sparkContext
-    main(salary_path, salary_player_path, player_summary_path, player_info_path, injury_path, output)
+    main(salary_path)
 
